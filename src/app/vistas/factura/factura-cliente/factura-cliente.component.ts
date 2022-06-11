213818df6263f4c59a0568bf830c1466fee2ba8c
Ezequiel Ferrreras <52600831+EzequielFerreras2@users.noreply.github.com>
2022-06-11 10:47:10 -0400

    Soporte11

--- src/app/vistas/factura/factura-cliente/factura-cliente.component.ts
+++ src/app/vistas/factura/factura-cliente/factura-cliente.component.ts
@@ -3,7 +3,9 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { ClienteI } from 'src/app/Models/Cliente/cliente.interface';
+import { VendedorI } from 'src/app/Models/Vendedor/vendedor.interface';
 import { ApiService } from 'src/app/services/api.service';
+import { VendedorService } from 'src/app/services/vendedor/vendedor.service';
 
 @Component({
   selector: 'factura-cliente',
@@ -12,9 +14,19 @@ import { ApiService } from 'src/app/services/api.service';
 })
 export class FacturaClienteComponent implements OnInit {
 
-  constructor(private activeroute:ActivatedRoute, private router: Router, private api:ApiService, private toast:ToastrService) { }
+  constructor(private activeroute:ActivatedRoute, private router: Router, private api:ApiService, private toast:ToastrService, private apiV: VendedorService) { }
 
   datosCliente!: ClienteI;
+  vendedores!: VendedorI[];
+  datosVendedor!:VendedorI;
+
+  vendedorForm= new FormGroup({
+
+    id: new FormControl(''), 
+    nombreVendedor: new FormControl('',[Validators.required]),
+    ventas: new FormControl('',[Validators.required])
+
+  });
 
 
 
@@ -25,7 +37,26 @@ export class FacturaClienteComponent implements OnInit {
     this.api.GetClienteByid(clienteId).subscribe( data =>{
        this.datosCliente = data;
 
-    })
+    }),
+
+    this.apiV.getAllVendedor().subscribe
+    
+    (data=>{
+
+      this.vendedores = data;
+     },  
+
+     (error) =>{
+       console.log(error)
+         this.toast.warning(`${error}`,'! Error')
+     }
+     
+     
+     );
+    
+
+     console.log(this.datosVendedor)
+
   }
 
 }
